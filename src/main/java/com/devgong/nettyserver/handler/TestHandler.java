871dc7ea2b9bf75fc7422d0b7585ef3e545dd4a8
gtgong <gtgong@scsol.co.kr>
2022-06-07 17:47:39 +0900

    feat : Handler 작업

--- src/main/java/com/devgong/nettyserver/handler/TestHandler.java
+++ src/main/java/com/devgong/nettyserver/handler/TestHandler.java
@@ -30,22 +30,24 @@ public class TestHandler extends ChannelInboundHandlerAdapter {
     // 핸들러가 제거될 때 호출되는 메소드
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) {
+        // 클라이언트와 연결되어 트래픽을 생성할 준비가 되었을 때 호출되는 메소드
         buff = null;
     }
 
-    // 클라이언트와 연결되어 트래픽을 생성할 준비가 되었을 때 호출되는 메소드
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
         String remoteAddress = ctx.channel().remoteAddress().toString();
         log.info("Remote Address: " + remoteAddress);
     }
 
-
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // 클라이언트 -> 서버, 날린 데이터를 받아야하는부분
         ByteBuf mBuf = (ByteBuf) msg;
-        System.out.println(ctx);
+//        System.out.println(ctx);
+//        System.out.println(mBuf);
         buff.writeBytes(mBuf);  // 클라이언트에서 보내는 데이터가 축적됨
+
         mBuf.release();
 
         final ChannelFuture f = ctx.writeAndFlush(buff);
