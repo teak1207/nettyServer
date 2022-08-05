e8068401a8d97578d86e63212f57ad4f732940ca
gtgong <gtgong@scsol.co.kr>
2022-08-05 17:52:18 +0900

    Feat : table [create] 최신화

--- src/main/java/com/devgong/nettyserver/util/InitTable.sql
+++ src/main/java/com/devgong/nettyserver/util/InitTable.sql
@@ -1,101 +1,47 @@
---================================
+-- ================================
 -- CREATE
-/*CREATE TABLE LEAKSET_BYSENSOR
+-- auto-generated definition
+create table factory_leak_project
 (
-    CID         INT AUTO_INCREMENT,
-    SN          VARCHAR(32),
-    REGDATE     VARCHAR(20),
-    INSERTID    VARCHAR(16),
-    IP          VARCHAR(32),
-    SID         VARCHAR(32),
-    ID          VARCHAR(32),
-    TIMEOPT1    VARCHAR(8),
-    STIMEOPT2   VARCHAR(8),
-    STIMEOPT3   VARCHAR(8),
-    FM          VARCHAR(8),
-    SLEEP       VARCHAR(1),
-    DEVICERESET VARCHAR(1),
-    PERIOD      VARCHAR (8),
-    PNAME       VARCHAR(32),
-    TDATE       VARCHAR(20),
-    SAMPLE      VARCHAR(2),
-    VALID       VARCHAR(1),
-    SAMPLERATE  VARCHAR(8),
-    FMTIME      VARCHAR(8),
-    BAUDRATE    VARCHAR(8),
-    FRAMESIZE   VARCHAR(2),
-
-    PRIMARY KEY (CID)
-);*/
-
-CREATE TABLE SENSOR_LIST_ALL
-(
-    CID      INT AUTO_INCREMENT,
-    REGDATE  varchar(30),
-    SSN      VARCHAR(32),
-    ASID     VARCHAR(32),
-    APROJECT VARCHAR(32),
-    MPPHONE  VARCHAR(24),
-    PRIMARY KEY (CID)
+    cid           int auto_increment
+        primary key,
+    factory_pname varchar(32) null,
+    data_URL      varchar(32) null,
+    data_PORT     varchar(8)  null,
+    db_URL        varchar(32) null,
+    db_PORT       varchar(8)  null
 );
 
-CREATE TABLE LEAK_PROJECT
-(
-    CID         INT AUTO_INCREMENT,
-    SID         VARCHAR(32),
-    PNAME       VARCHAR(64),
-    DATA_SERVER VARCHAR(32),
-    DATA_PORT   VARCHAR(8),
-    PRIMARY KEY (CID)
-);
 
-CREATE TABLE LEAKSET_BYSENSOR
+-- auto-generated definition
+create table sensor_list_all
 (
-    CID        INT AUTO_INCREMENT,
-    SN         VARCHAR(32),
-    TIMEOPT1   VARCHAR(8),
-    TIMEOPT2   VARCHAR(8),
-    TIMEOPT3   VARCHAR(8),
-    PERIOD     VARCHAR (8),
-    SAMPLE     VARCHAR(2),
-    SAMPLERATE VARCHAR(8),
-    PRIMARY KEY (CID)
-);
+    CID      int auto_increment
+        primary key,
+    REGDATE  varchar(30) null,
+    SSN      varchar(32) null,
+    ASID     varchar(32) null,
+    APROJECT varchar(32) null,
+    MPHONE   varchar(24) null,
+    FRESET   varchar(1)  null
+)
+    auto_increment = 5;
 
-create table factory_report
+
+
+-- auto-generated definition
+create table leak_project
 (
-    cid          int auto_increment,
-    serialNumber VARCHAR(64) null,
-    REPORTTIME   DATETIME    null,
-    ID           VARCHAR(16) null,
-    IP           VARCHAR(36) null,
-    PX           VARCHAR(16) null,
-    SID          VARCHAR(64) null,
-    PY           VARCHAR(16) null,
-    PNAME        VARCHAR(64) null,
-    TIME1        VARCHAR(8)  null,
-    TIME2        VARCHAR(8)  null,
-    TIME3        VARCHAR(8)  null,
-    DEBUG        VARCHAR(20) null,
-    FM           VARCHAR(8)  null,
-    FVER         VARCHAR(8)  null,
-    RSSI         VARCHAR(8)  null,
-    STATUS_TEMP  VARCHAR(2)  null,
-    PERIOD_TEMP  VARCHAR(2)  null,
-    BATTERY      VARCHAR(8)  null,
-    PROJECT      VARCHAR(32) null,
-    DATA_SERVER  VARCHAR(32) null,
-    DATA_PORT    VARCHAR(8)  null,
-    DB_SERVER    VARCHAR(32) null,
-    DB_PORT      VARCHAR(8)  null,
-    REPORT_CHK   VARCHAR(16) null,
-    FMTIME       VARCHAR(8)  null,
-    BAUDRATE     VARCHAR(8)  null,
-    PCBVERSION   VARCHAR(8)  null,
-    ACKNAK       VARCHAR(4)  null,
-    constraint factory_report_pk
-        primary key (cid)
-);
+    CID         int auto_increment
+        primary key,
+    SID         varchar(32) null,
+    PNAME       varchar(64) null,
+    DATA_SERVER varchar(32) null,
+    DATA_PORT   varchar(8)  null,
+    DB_URL      varchar(32) null,
+    DB_PORT     varchar(8)  null
+)
+    auto_increment = 3;
 
 -- auto-generated definition
 create table leakset
@@ -112,17 +58,119 @@ create table leakset
     active         varchar(1) null
 );
 
+-- auto-generated definition
+create table factory_report
+(
+    cid           int auto_increment
+        primary key,
+    SERIALNUMBER  varchar(64) null,
+    REPORTTIME    varchar(20) null,
+    DEBUG         varchar(20) null,
+    TIME1         varchar(8)  null,
+    TIME2         varchar(8)  null,
+    TIME3         varchar(8)  null,
+    FM_RADIO      varchar(8)  null,
+    FVER          varchar(8)  null,
+    BATTERY       varchar(8)  null,
+    RSSI          varchar(8)  null,
+    DEVICE_STATUS varchar(2)  null,
+    SAMPLINGTIME  varchar(20) null,
+    PX            varchar(16) null,
+    PY            varchar(16) null,
+    MODEMNUMBER   varchar(50) null,
+    SID           varchar(64) null,
+    PERIOD varchar (2) null,
+    SERVER_URL    varchar(32) null,
+    SERVER_PORT   varchar(8)  null,
+    DB_URL        varchar(32) null,
+    DB_PORT       varchar(8)  null,
+    BAUDRATE      varchar(1)  null,
+    PCB_VER       varchar(1)  null
+);
 
+-- auto-generated definition
 create table factory_sensor_list
 (
-    cid          int auto_increment,
+    cid          int auto_increment
+        primary key,
     sid          varchar(64) null,
     pname        varchar(64) null,
     sn           varchar(64) null,
-    factorypname varchar(32) null,
-    constraint factory_sensor_list_pk
-        primary key (cid)
+    factorypname varchar(32) null
 );
+
+
+-- auto-generated definition
+create table leakmaster_init_infos
+(
+    ID       int auto_increment
+        primary key,
+    NAME     varchar(32) not null,
+    SID      varchar(32) not null,
+    SN       varchar(32) not null,
+    SIZE     int         not null,
+    PHONENUM varchar(32) not null,
+    REGDATE  varchar(32) not null
+)
+    auto_increment = 3;
+
+
+-- auto-generated definition
+create table leakset
+(
+    cid          int auto_increment
+        primary key,
+    time1        varchar(8)  null,
+    time2        varchar(8)  null,
+    time3        varchar(8)  null,
+    fm_frequency varchar(8)  null,
+    sid          varchar(32) null,
+    pname        varchar(32) null,
+    sleep        varchar(1)  null,
+    reset        varchar(1)  null,
+    period       varchar (8) null,
+    sampletime   varchar(2)  null,
+    active       varchar(1)  null,
+    samplerate   varchar(8)  null,
+    fmtime       varchar(8)  null
+)
+    auto_increment = 2;
+
+-- auto-generated definition
+create table leakset_bysensor
+(
+    CID          int auto_increment
+        primary key,
+    SN           varchar(32) null,
+    TIMEOPT1     varchar(8)  null,
+    TIMEOPT2     varchar(8)  null,
+    TIMEOPT3     varchar(8)  null,
+    FM_FREQUENCY varchar(8)  null,
+    PERIOD varchar (8) null,
+    SAMPLE       varchar(2)  null,
+    SAMPLERATE   varchar(8)  null,
+    RADIO_TIME   varchar(8)  null,
+    BAUDRATE     varchar(8)  null
+)
+    auto_increment = 3;
+
+
+
+-- auto-generated definition
+create table sensor_list
+(
+    cid   int auto_increment
+        primary key,
+    sn    varchar(64) null,
+    px    varchar(16) null,
+    py    varchar(16) null,
+    pname varchar(64) null,
+    sid   varchar(64) null
+)
+    auto_increment = 3;
+
+
+
 
 
 -- ================================
@@ -133,7 +181,7 @@ VALUES (1, 'thingsware.co.kr', '8998');
 
 INSERT INTO leak_project
 VALUES (1, 'SWFLB-20200312-0102-0004', '2020-03-17 13:12:01', '', '218.155.80.145', 'scleak', '0200', '0300', '0400',
-        '099', '2', '2', '0', '20200310_20', '-1', '5',);
+        '099', '2', '2', '0', '20200310_20', '-1', '5',0);
 
 INSERT INTO sensor_list_all
 VALUES (1, now(), 'SWSLB-20220530-0000-0001', 'producttest', 'tesk_chk', '8212-3266-1739');
