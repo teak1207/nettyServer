5d8232c1d4e424242f682cceab4c98c3b39e51d6
gong <teak1207@gmail.com>
2022-10-14 16:16:01 +0900

    report process, table not exist issue(31),형변환중

--- src/main/java/com/devgong/nettyserver/protocol/Report/ReportModifyData.java
+++ src/main/java/com/devgong/nettyserver/protocol/Report/ReportModifyData.java
@@ -1,13 +1,15 @@
 package com.devgong.nettyserver.protocol.Report;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class ReportModifyData {
 
 
-    public Object convertData(char value) {
+    public int convertData(char value) {
 
         String convertedHex = Integer.toHexString(value);
 
