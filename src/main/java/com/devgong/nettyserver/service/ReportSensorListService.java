4b5064e2cac5ab750e693f4900e309e51b30a070
gong <teak1207@gmail.com>
2022-10-14 11:10:41 +0900

    report process, table not exist issue(22),casting

--- src/main/java/com/devgong/nettyserver/service/ReportSensorListService.java
+++ src/main/java/com/devgong/nettyserver/service/ReportSensorListService.java
@@ -53,7 +53,7 @@ public class ReportSensorListService {
 
 
         log.info("request check : {}", request);
-        log.info("fuck : {}", (request.getParameter().getRssi()));
+        log.info("fuck : {}", Character.getNumericValue(request.getParameter().getRssi().charAt(0)));
 
         dataInsertModel.setSn(request.getSensorId());
         dataInsertModel.setEndRecordTime(request.getParameter().getEndRecordTime());
