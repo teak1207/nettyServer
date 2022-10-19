f6d7281024c7bade368fc87f139e4cd554b8a0f8
gong <teak1207@gmail.com>
2022-10-19 16:20:25 +0900

    data process , reqeust(7)

--- src/main/java/com/devgong/nettyserver/protocol/PacketFlag.java
+++ src/main/java/com/devgong/nettyserver/protocol/PacketFlag.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PacketFlag {
     REQUEST((byte) '4'),
-    DATA((byte) 'S'),
+    DATA((byte) '5'),
     SETTING((byte) '6'),
     REPORT((byte) '7'),
     ACK((byte) '8'),
