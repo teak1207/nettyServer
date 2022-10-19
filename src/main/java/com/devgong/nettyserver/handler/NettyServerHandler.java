ab6ce1d8bdd98f4c7f7aa58c57d8daf7d833db06
gong <teak1207@gmail.com>
2022-10-19 16:27:57 +0900

    data process , reqeust(10)

--- src/main/java/com/devgong/nettyserver/handler/NettyServerHandler.java
+++ src/main/java/com/devgong/nettyserver/handler/NettyServerHandler.java
@@ -294,7 +294,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
             } else if (PacketFlag.DATA.equals(flag)) {
 
                 log.info("flag : {}" , flag);
-                log.info("엄석");
+                log.info("뭐가문제야");
 
 
 
