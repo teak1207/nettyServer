04f8e2d645affc13fa6afd70218cc1ba6b228c29
gong <teak1207@gmail.com>
2022-10-19 21:20:14 +0900

    data process , reqeust(25)

--- src/main/java/com/devgong/nettyserver/socket/NettyChannelInitializer.java
+++ src/main/java/com/devgong/nettyserver/socket/NettyChannelInitializer.java
@@ -23,13 +23,14 @@ public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel ch) {
         ChannelPipeline pipeline = ch.pipeline();
 
+
+        ch.config().setReceiveBufferSize(1024);
         // decoder는 @Sharable이 안 됨, Bean 객체 주입이 안 되고, 매번 새로운 객체 생성해야 함
         TestDecoder testDecoder = new TestDecoder();
 
         pipeline.addLast(nettyServerHandler);
         pipeline.addLast(new DelimiterBasedFrameDecoder(2048, Delimiters.lineDelimiter()));
 
-        // 이게 뭘까?
 
 //        pipeline.addLast(new StringEncoder());
         // 뒤이어 처리할 디코더 및 핸들러 추가
