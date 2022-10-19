7b20c587850854f5118aa995458d242d007365d6
gong <teak1207@gmail.com>
2022-10-19 13:51:03 +0900

    request process, confirmPath check(29)

--- src/main/java/com/devgong/nettyserver/service/RequestSensorListService.java
+++ src/main/java/com/devgong/nettyserver/service/RequestSensorListService.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Service;
 
 import javax.xml.bind.DatatypeConverter;
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
@@ -61,11 +63,10 @@ public class RequestSensorListService {
         } else {
             String defaultPath = "/home/scsol/public_html/leak_data_gong/";
             String path = defaultPath + requestFindResults.getAsid() + "/" + requestFindResults.getAproject() + "/" + requestFindResults.getSsn();
+
             log.info("path : {}", path);
 
             char underBar = '_';
-
-            log.info("initDate : {}", convertDate(request.getDateTime()));
 
 
             String filePath = path + "/" + requestFindResults.getSsn() + underBar + convertDate(request.getDateTime()) + underBar + convertedSampleRate + ".dat";
@@ -78,14 +79,24 @@ public class RequestSensorListService {
 
             log.info("dataRefModel: {}", dataRefModel.getFilepath());
 
-            if (file3.isDirectory()) {
-                log.info("good");
-            } else {
-                log.info("fail");
 
+            if (Files.exists(filePathExistence)) {
+
+                if (file3.isDirectory()) {
+                    //memo : 해당 경로에 파일 존재할 경우, NAK return
+                    log.info("경로가 존재합니다. : {}", filePath);
+                } else {
+                    //memo : 해당 경로에 파일이 없을 경우,File 생성후, ACK return
+                    log.info("경로가 존재하지 않습니다. : {}", filePath);
+                    try {
+                        if (initFilePath.createNewFile()) {
+                            log.info("생성완료 : {} ", filePath);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-
-
         }
 
     }
