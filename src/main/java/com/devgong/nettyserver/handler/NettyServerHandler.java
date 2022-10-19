47c633a92feaa787d91db9cb73d4e59aca5fb605
gong <teak1207@gmail.com>
2022-10-19 20:02:59 +0900

    data process , reqeust(22)

--- src/main/java/com/devgong/nettyserver/handler/NettyServerHandler.java
+++ src/main/java/com/devgong/nettyserver/handler/NettyServerHandler.java
@@ -297,8 +297,6 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
                 }
 
 
-
-
             } else if (PacketFlag.DATA .equals(flag)) {
 
                 log.info("flag : {}" , flag);
@@ -311,18 +309,12 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
                 log.info("Data  readable bytes length : {}", bytes.length);
                 log.info("Data FLAG : {}", (char) readFlag);
 
-                int i =0;
-                for(byte s : bytes){
-                    log.info("data : {}", s);
-                    log.info("data : {}", (char)s);
-                    log.info("data",i++);
-                    log.info("-------------------");
-                }
-
-
-
+//                for(byte s : bytes){
+//                    log.info("data : {}", s);
+//                    log.info("data : {}", (char)s);
+//                    log.info("-------------------");
+//                }
             }
-
 
         } catch (Exception e) {
             e.printStackTrace();
