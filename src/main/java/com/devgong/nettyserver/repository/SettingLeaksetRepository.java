4dc1adf36d27661342132846eb34702958c97f47
gong <teak1207@gmail.com>
2022-10-25 10:28:26 +0900

    SettingSensorListRepository , 옵션 추가(8)

--- src/main/java/com/devgong/nettyserver/repository/SettingLeaksetRepository.java
+++ src/main/java/com/devgong/nettyserver/repository/SettingLeaksetRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface SettingLeaksetRepository extends JpaRepository<SettingLeaksetModel, Integer> {
     SettingLeaksetModel findAllBySidAndPnameAndReset(String sid, String pname,String freset);
 
-    SettingLeaksetModel findTop1BySidAndPnameOrderByCidDesc  (String sid, String pname,String ssn);
+    SettingLeaksetModel findTop1BySidAndPnameAndResetOrderByCidDesc  (String sid, String pname,String freset);
 
 }
