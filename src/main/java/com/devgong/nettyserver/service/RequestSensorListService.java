b98c87fd491d6519c8d8c49e0cab718768388b02
teak1207 <teak1207@gmail.com>
2022-11-30 13:32:22 +0900

    전송데이터 크기 변화 테스트(10),원복

--- src/main/java/com/devgong/nettyserver/service/RequestSensorListService.java
+++ src/main/java/com/devgong/nettyserver/service/RequestSensorListService.java
@@ -81,21 +81,10 @@ public class RequestSensorListService {
 
         byte[] temp = request.getParameter().getFrameCount().getBytes(StandardCharsets.UTF_8);
 
-        byte value = temp[1];
-        int nValue = 0;
 
-        if(value < 0){
-            nValue = (int)value+256;
-        }else{
-            nValue =(int)value;
-        }
+        String frameCount = String.valueOf(temp[1]);
 
-//        String frameCount = String.valueOf(temp[1]);
-        String frameCount = Integer.toString(nValue);
-
-
-
-        log.info("parseInt test: {}", Integer.parseInt(frameCount));
+        log.info("parseInt test: {}",Integer.parseInt(frameCount) );
 
         requestLeakDataModel.setPname(sensorListAll.getAproject());
         requestLeakDataModel.setDate((simpleDateFormat.format(now)));
