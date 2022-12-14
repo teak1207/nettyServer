7f1f3d2c4c8ad34e0e7f5c19d14b1daba00caaf0
teak1207 <teak1207@gmail.com>
2022-12-14 14:17:43 +0900

    전송데이터 크기 변화 테스트(79)

--- src/main/java/com/devgong/nettyserver/service/RequestSensorListService.java
+++ src/main/java/com/devgong/nettyserver/service/RequestSensorListService.java
@@ -97,6 +97,7 @@ public class RequestSensorListService {
         log.info("sc1 : {}", sampleRate);
         log.info("sc2 : {}", byteArrayToHex(sampleRate));
         log.info("temp len : {}", sampleRate.length);
+        log.info("--------------------------------------");
 //        log.info("sc : {}", bytesToInt(sampleRate));
 
 
