ad755325293360cb23606a72044dab2e72fbbd43
teak1207 <teak1207@gmail.com>
2023-01-26 17:13:46 +0900

    freset check(6)

--- src/main/java/com/devgong/nettyserver/handler/NettyServerHandler.java
+++ src/main/java/com/devgong/nettyserver/handler/NettyServerHandler.java
@@ -218,7 +218,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
                 if (settingDeviceInfos.isPresent()) {
                     SettingResponseModel deviceInfo = settingDeviceInfos.get();
 
-                    log.info("freset456: {}", settingDeviceInfos.toString());
+                    log.info("freset456: {}", deviceInfo);
 
                     SettingResponse response = new SettingResponse(
                             deviceInfo.getTime1(),
