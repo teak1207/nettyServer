5aaa69377800af99e3401b09c14316ec44b31b84
teak1207 <teak1207@gmail.com>
2023-01-26 15:14:31 +0900

    Jpa, 쿼리 예외처리

--- src/main/java/com/devgong/nettyserver/repository/PreInstallSensorListAllRepository.java
+++ src/main/java/com/devgong/nettyserver/repository/PreInstallSensorListAllRepository.java
@@ -1,12 +1,14 @@
 package com.devgong.nettyserver.repository;
 
 import com.devgong.nettyserver.domain.PreInstallSensorListAllModel;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PreInstallSensorListAllRepository extends JpaRepository<PreInstallSensorListAllModel, Integer> {
-    PreInstallSensorListAllModel findPreInstallModelBySsn(String sn);
+
+    PreInstallSensorListAllModel findPreInstallModelBySsn(String sn) throws DataAccessException;
 
     PreInstallSensorListAllModel findPreInstallSensorListAllModelByMphone(String modemNum);
 }
