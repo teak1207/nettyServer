9d38ba82e47f1fff4ac36b333e4980500da649f5
teak1207 <teak1207@gmail.com>
2023-02-20 13:54:04 +0900

    미배치센서, 쿼리 수정

--- src/main/java/com/devgong/nettyserver/repository/SettingSensorListRepository.java
+++ src/main/java/com/devgong/nettyserver/repository/SettingSensorListRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SettingSensorListRepository extends JpaRepository<SettingSensorListModel, Integer> {
-    @Query("select m from SettingSensorListModel AS m where m.sid = :sid and m.pname= :pname and m.serialNumber= :serialNumber  and NOT m.col_valid = -1")
+    @Query("select m from SettingSensorListModel AS m where m.sid = :sid and m.pname= :pname and m.serialNumber= :serialNumber  and NOT m.col_valid = -1 order by m.cid desc ")
     SettingSensorListModel findBySidAndPnameAndSerialNumber(String sid, String pname, String serialNumber);
 }
